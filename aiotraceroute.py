28e30c8a69877fe4784be97de626ad2d454d3d5a
Thomas Desrosiers <git@hive.pw>
2021-02-28 13:28:17 -0500

    Fix concurrency issues in aiotraceroute

--- aiotraceroute.py
+++ aiotraceroute.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
-import time
-import socket
 import asyncio
-import aiodns
+from functools import cached_property
+import secrets
+import socket
+import struct
+import time
+from typing import Union
 
 
 def aiotraceroute(dest, port=33434, max_hops=30, timeout=1, packet_size=60):
     return _AsyncTraceroute(dest, port, max_hops, timeout, packet_size)
+
+
+_wireformat_port = struct.Struct("!H")
 
 
 class _AsyncTraceroute:
@@ -24,24 +30,29 @@ class _AsyncTraceroute:
 
         try:
             socket.inet_aton(dest)
-            self.dest_addr = dest
+            self.dst_addr = dest
         except socket.error:
-            self.dest_addr = socket.gethostbyname(dest)
+            self.dst_addr = socket.gethostbyname(dest)
 
-        self.port = port
-        self.max_hops = max_hops
-        self.timeout = timeout
-        self.packet_size = packet_size
-        self.i = 0
-        self._ttl = 0
+        self.starting_port: int = port
+        self.max_hops: int = max_hops
+        self.timeout: Union[int, float] = timeout
+        self.payload: bytes = secrets.token_bytes(packet_size)
+        self._ttl: int = 1
+
         self._loop = asyncio.get_event_loop()
-        self._resolver = aiodns.DNSResolver(loop=self._loop)
         self._queue = asyncio.Queue()
         self._rx = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
         self._tx = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        self._loop.add_reader(
-            self._rx, lambda: self._queue.put_nowait(self._rx.recvfrom(512))
-        )
+        self._loop.add_reader(self._rx, self._handle_response)
+
+    @property
+    def src_port(self):
+        return self._tx.getsockname()[1]
+
+    @property
+    def dst_port(self) -> int:
+        return self.starting_port + self._ttl
 
     async def run(self):
         return [res async for res in self]
@@ -61,27 +72,57 @@ class _AsyncTraceroute:
             raise StopAsyncIteration
 
         self._ttl += 1
-        self.i += 1
         next_addr = None
 
         try:
             self._tx.setsockopt(socket.SOL_IP, socket.IP_TTL, self._ttl)
 
             start = stop = time.perf_counter_ns()
-            self._tx.sendto(b"X" * self.packet_size, (self.dest_addr, self.port))
+
+            self._tx.sendto(self.payload, (self.dst_addr, self.dst_port))
+
             try:
-                _, addr = await asyncio.wait_for(self._queue.get(), self.timeout)
+                _, (next_addr, _) = await asyncio.wait_for(
+                    self._queue.get(), self.timeout
+                )
                 stop = time.perf_counter_ns()
-                next_addr = addr[0]
-                if next_addr == self.dest_addr:
-                    self._ttl = self.max_hops
+
+                if next_addr == self.dst_addr:
+                    # Stop on next iteration
+                    self.max_hops = self._ttl
+
                 result = "success"
             except asyncio.TimeoutError:
                 result = "timeout"
 
-            return self.i, result, next_addr, (stop - start) // 1000
+            return self._ttl, result, next_addr, (stop - start) // 1000
         except Exception as e:
             # Stop on next iteration
-            self._ttl = self.max_hops
+            self.max_hops = self._ttl
 
-            return (self.i, str(e), None, None)
+            return (self._ttl, repr(e), None, None)
+
+    def _handle_response(self):
+        payload, src = self._rx.recvfrom(4096)
+
+        if self._should_handle_response(payload):
+            self._queue.put_nowait((payload, src))
+
+    def _should_handle_response(self, payload):
+        # Nonce came back, handle packet
+        if self.payload in payload:
+            return True
+
+        # If we don't get our unique payload back [1], we'll need to try and
+        # match on the fields in the return IP & UDP payload.
+        #
+        # [1]: RFC1812 only mandates the first 8 bytes of the IP payload be
+        # returned. Which in most cases is just the headers of the next protocol
+        # (in our case UDP).
+
+        same_dst_addr = socket.inet_aton(self.dst_addr) == payload[44:48]
+
+        same_src_port = _wireformat_port.pack(self.src_port) == payload[48:50]
+        same_dst_port = _wireformat_port.pack(self.dst_port) == payload[50:52]
+
+        return same_src_port and same_dst_addr and same_dst_port
